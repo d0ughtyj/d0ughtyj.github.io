495a05d49cf7f7d64c88692c73d76296d959cd5f
Jim Doughty <jim.doughty.colorado@gmail.com>
2017-05-26 09:15:40 -0600

    portfolio update

--- portfolio/bin/app.js
+++ portfolio/bin/app.js
@@ -1,1 +1,12 @@
 console.log('load app.js');
+
+// $(document).ready(function(){
+//      $("#myCarousel").carousel({
+//        interval: false
+//      });
+// });
+
+
+// $('#myCarousel').on('slide.bs.carousel', function () {
+//   // do something…
+// });
---
+++ portfolio/bin/my_slick.js
@@ -0,0 +1,10 @@
+// http://kenwheeler.github.io/slick/
+
+
+$(document).ready(function(){
+  $('.clsSlick').slick({
+    infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1
+  });
+});
