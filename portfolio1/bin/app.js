f0b629245484c04aef1de48c18bb684adcbbc2c8
Jim Doughty <jim.doughty.colorado@gmail.com>
2017-05-30 12:44:51 -0600

    tuesday update

---
+++ portfolio1/bin/app.js
@@ -0,0 +1,12 @@
+console.log('load app.js');
+
+// $(document).ready(function(){
+//      $("#myCarousel").carousel({
+//        interval: false
+//      });
+// });
+
+
+// $('#myCarousel').on('slide.bs.carousel', function () {
+//   // do something…
+// });
